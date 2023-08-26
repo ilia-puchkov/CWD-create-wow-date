25bc227499c2e910fc373718deba092c968b6029
ilia-puchkov <reras@yandex.ru>
2023-08-26 15:17:20 +0300

    dev: изменение галереи

--- src/components/ImageSlider/ImagesSlider.js
+++ src/components/ImageSlider/ImagesSlider.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 
 function ImageSlider({ slides, onImageClick }) {
   // Gallery indexes
-  const [currentIndex, setCurrentIndex] = useState(1);
-  const [secondIndex, setSecondIndex] = useState(2);
-  const [thirdIndex, setThirdIndex] = useState(3);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [secondIndex, setSecondIndex] = useState(1);
+  const [thirdIndex, setThirdIndex] = useState(2);
+  const [fourthIndex, setFourthIndex] = useState(3);
 
   function goToPrevious() {
     const isFirstSlide = currentIndex === 0;
@@ -12,10 +13,13 @@ function ImageSlider({ slides, onImageClick }) {
     const newSecondIndex = isFirstSlide ? 0 : newIndex + 1;
     const newThirdIndex =
       newSecondIndex === slides.length - 1 ? 0 : newSecondIndex + 1;
+    const newFourthIndex =
+      newThirdIndex === slides.length - 1 ? 0 : newThirdIndex + 1;
 
     setCurrentIndex(newIndex);
     setSecondIndex(newSecondIndex);
     setThirdIndex(newThirdIndex);
+    setFourthIndex(newFourthIndex);
   }
 
   function goToNext() {
@@ -24,10 +28,13 @@ function ImageSlider({ slides, onImageClick }) {
     const newSecondIndex = newIndex === slides.length - 1 ? 0 : newIndex + 1;
     const newThirdIndex =
       newSecondIndex === slides.length - 1 ? 0 : newSecondIndex + 1;
+    const newFourthIndex =
+      newThirdIndex === slides.length - 1 ? 0 : newThirdIndex + 1;
 
     setCurrentIndex(newIndex);
     setSecondIndex(newSecondIndex);
     setThirdIndex(newThirdIndex);
+    setFourthIndex(newFourthIndex);
   }
 
   function goToSlide(slideIndex) {
@@ -35,10 +42,13 @@ function ImageSlider({ slides, onImageClick }) {
       slideIndex === slides.length - 1 ? 0 : slideIndex + 1;
     const newThirdIndex =
       newSecondIndex === slides.length - 1 ? 0 : newSecondIndex + 1;
+    const newFourthIndex =
+      newThirdIndex === slides.length - 1 ? 0 : newThirdIndex + 1;
 
     setCurrentIndex(slideIndex);
     setSecondIndex(newSecondIndex);
     setThirdIndex(newThirdIndex);
+    setFourthIndex(newFourthIndex);
   }
 
   function handleImageClick(e) {
@@ -60,16 +70,24 @@ function ImageSlider({ slides, onImageClick }) {
           alt={slides[currentIndex].title}
           onClick={handleImageClick}
         ></img>
+        <div className='slider__middle'>
         <img
-          className='slider__element slider2'
+          className='slider__element slider__middle-image slider2'
           src={slides[secondIndex].url}
           alt={slides[secondIndex].title}
           onClick={handleImageClick}
         ></img>
         <img
-          className='slider__element slider3'
+          className='slider__element slider__middle-image slider3'
           src={slides[thirdIndex].url}
           alt={slides[thirdIndex].title}
+          onClick={handleImageClick}
+        ></img>
+        </div>        
+        <img
+          className='slider__element slider4'
+          src={slides[fourthIndex].url}
+          alt={slides[fourthIndex].title}
           onClick={handleImageClick}
         ></img>
       </div>
