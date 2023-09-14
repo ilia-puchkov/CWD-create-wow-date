37388c74e1a3a2cf2e58ef6960b2b5bce8e5a5d8
ilia-puchkov <reras@yandex.ru>
2023-09-14 13:30:20 +0300

    fix: закрытие картинки по клику

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -1,6 +1,6 @@
 function ImagePopup(props) {
   return (
-    <div className={`popup popup_type_full-image ${props.isOpen ? "popup_opened" : ""}`}>
+    <div className={`popup popup_type_full-image ${props.isOpen ? "popup_opened" : ""}`} onClick={props.onClose}>
       <div className="popup__image-container">
         <img className="popup__image" src={props.image.src} alt={props.image.alt} />
         <p className="popup__signature">{props.image.alt}</p>
